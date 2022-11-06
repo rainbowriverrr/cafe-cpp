e415cfe39ff52d1980d9c88cd0c856e61d4cc49c
River Wang <friver980@gmail.com>
2022-11-05 20:16:53 -0400

    menu item factory test with comments

--- src/web/MenuWidgets.cpp
+++ src/web/MenuWidgets.cpp
@@ -5,6 +5,15 @@
 
 #include "MenuWidgets.hpp"
 
+/*
+    MenuItem
+        Parameters:
+            name: name of the item
+            price: price of the item
+            description: description of the item
+            cartButton: pointer to the cart button (still not sure how to do this so i haven't implemented it yet)
+    This method creates a MenuItem object, which is a container widget that contains the name, price, description, and cart button of an item
+*/
 MenuItem::MenuItem(const std::string &name, const std::string &price, const std::string &description, const Wt::WPushButton *cartButton) {
     setContentAlignment(Wt::AlignmentFlag::Center);
     name_ = addWidget(std::make_unique<Wt::WText>(name));
@@ -13,6 +22,12 @@ MenuItem::MenuItem(const std::string &name, const std::string &price, const std:
     cartButton_ = addWidget(std::make_unique<Wt::WPushButton>("Add to Cart"));
 }
 
+/*
+    getCartPtr
+        Return:
+            WT::PushButton * : pointer to the cart button
+    This method returns the pointer to the cart button, allowing you to connect it to a function
+*/
 Wt::WPushButton *MenuItem::getCartPtr() {
     return cartButton_;
 }
