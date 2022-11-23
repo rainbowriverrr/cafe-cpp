6ee7959f23b5d683b75e32ac45e54c33e0d66764
Alex Mihas <amihas@uwo.ca>
2022-11-22 21:59:10 -0500

    added log-in page

---
+++ src/web/LoginPage.cpp
@@ -0,0 +1,8 @@
+//
+//  LoginPage.cpp
+//  GroupProject
+//
+//  Created by Alex Mihas on 2022-11-20.
+//
+
+#include <stdio.h>
---
+++ src/web/LoginPage.hpp
@@ -0,0 +1,12 @@
+//
+//  LoginPage.h
+//  GroupProject
+//
+//  Created by Alex Mihas on 2022-11-20.
+//
+
+#ifndef LoginPage_h
+#define LoginPage_h
+
+
+#endif /* LoginPage_h */
