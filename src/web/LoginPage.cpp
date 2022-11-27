2e51c6254895bcd7ab01aa9b2a65f4517ab47087
Alex Mihas <amihas@uwo.ca>
2022-11-27 17:00:39 -0500

    updated log-in form

--- src/web/LoginPage.cpp
+++ src/web/LoginPage.cpp
@@ -13,25 +13,32 @@
 
 LoginPage::LoginPage() {
     
-    auto container = std::make_unique<Wt::WContainerWidget>();
-    Wt::WContainerWidget *listWidget;
+    
+    auto text
+      = addNew<Wt::WText>("<p>Please enter your username and password.</p>");
     
     
+    auto result = addNew<Wt::WTemplate>(Wt::WString::tr("simpleForm-template"));
     
-    container->addNew<Wt::WRadioButton>("Radio me!");
-    auto result =
-        std::make_unique<Wt::WTemplate>(Wt::WString::tr("simpleForm-template"));
-
-    auto name = result->bindWidget("name", std::make_unique<Wt::WLineEdit>());
-    name->setPlaceholderText("first name");
-
-    auto button = result->bindWidget("button", std::make_unique<Wt::WPushButton>("OK"));
-
+    
+    auto username = result->bindWidget("user-name", std::make_unique<Wt::WLineEdit>());
+    username->setPlaceholderText("username");
+    
+    auto password = result->bindWidget("user-password", std::make_unique<Wt::WLineEdit>());
+    password->setPlaceholderText("password");
+    
+    auto button = result->bindWidget("button", std::make_unique<Wt::WPushButton>("Log In"));
+    
     auto out = result->bindWidget("out", std::make_unique<Wt::WText>());
+    
+
+    
+    
 
     button->clicked().connect([=] {
-        out->setText("Hello, " + name->text() + "! I just want to help you... You"
-                     + " could complete this simple form by adding validation.");
+        out->setText("Your username is " + username->text() + ". Your password is " + password->text());
+        
+        // authenticate
     });
 }
 
