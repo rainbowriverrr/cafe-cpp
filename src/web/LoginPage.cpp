068edd6e5898c122893ae48a31a5f4410ad89878
Alex Mihas <amihas@uwo.ca>
2022-11-27 14:30:08 -0500

    testing a simple form

--- src/web/LoginPage.cpp
+++ src/web/LoginPage.cpp
@@ -4,16 +4,35 @@
 */
 
 #include "LoginPage.hpp"
+#include <Wt/WLineEdit.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WTemplate.h>
+#include <Wt/WText.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WRadioButton.h>
 
 LoginPage::LoginPage() {
+    
+    auto container = std::make_unique<Wt::WContainerWidget>();
     Wt::WContainerWidget *listWidget;
     
-    listWidget = addNew<WContainerWidget>();
-    
-    listWidget->addNew<Wt::WTemplate>(tr("login-page"));
     
     
-    //Wt::WApplication::instance()->setInternalPath("/login", true);
+    container->addNew<Wt::WRadioButton>("Radio me!");
+    auto result =
+        std::make_unique<Wt::WTemplate>(Wt::WString::tr("simpleForm-template"));
+
+    auto name = result->bindWidget("name", std::make_unique<Wt::WLineEdit>());
+    name->setPlaceholderText("first name");
+
+    auto button = result->bindWidget("button", std::make_unique<Wt::WPushButton>("OK"));
+
+    auto out = result->bindWidget("out", std::make_unique<Wt::WText>());
+
+    button->clicked().connect([=] {
+        out->setText("Hello, " + name->text() + "! I just want to help you... You"
+                     + " could complete this simple form by adding validation.");
+    });
 }
 
 LoginPage::~LoginPage() {
