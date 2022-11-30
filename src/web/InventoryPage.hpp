4ae4fcbc3d72ae030546fdb9719ad8c12cc6100b
dmcgarr <dmcgarr@uwo.ca>
2022-11-29 23:38:05 -0500

    comments added

--- src/web/InventoryPage.hpp
+++ src/web/InventoryPage.hpp
@@ -1,8 +1,8 @@
-/*
-    Author : Daniel McGarr
-    Date   : 2022/11/22
-*/
-
+/**
+ * Class representing the Wt Inventory Page.
+ * @author Daniel McGarr
+ * @date 2022-10-28
+ */
 #ifndef InventoryPage_h
 #define InventoryPage_h
 
@@ -40,8 +40,21 @@ class InventoryPage : public Wt::WContainerWidget {
      */
     ~InventoryPage();
     
+    
+    /**
+     * @brief creates widges for each item in the inventory and displays them on the webpage
+     * @Param inventoryListContainer, is the widget container used for the Inventory Page
+     * @Param InventoryItem is the item to be displayed on the webpage
+     */
     std::unique_ptr<Wt::WTemplate> createListItemWidget(Wt::WContainerWidget *inventoryListContainer, InventoryItem inv);
     
+    /**
+     * @brief updates database and widget when the update button is pressed.
+     * @Param listItem is the WTemplate used to display the changes when the quantity is updated
+     * @Param inventoryListContainer is the widget container used for the inventory page
+     * @Param Inventory Item is the item being updated
+     * @Param qty is the line from the input, representing the new updated quantity.
+     */
     void onUpdateInventoryBtnClicked(Wt::WTemplate *listItem, Wt::WContainerWidget *inventoryListContainer, InventoryItem item, Wt::WLineEdit* const qty);
 };
 
